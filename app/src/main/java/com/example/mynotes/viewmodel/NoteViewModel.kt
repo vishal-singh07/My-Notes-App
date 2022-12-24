43c0cc2b858ef73772efe01b3e5ca358ae796830
Vishal Singh <69778583+vishal-singh07@users.noreply.github.com>
2022-12-24 10:04:48 +0530

    Update NoteViewModel.kt

--- app/src/main/java/com/example/mynotes/viewmodel/NoteViewModel.kt
+++ app/src/main/java/com/example/mynotes/viewmodel/NoteViewModel.kt
@@ -73,17 +73,6 @@ class NoteViewModel(private val noteDao: NoteDAO) : ViewModel() {
         }
         return true
     }
-    /*
-    class InventoryViewModelFactory(private val itemDao: ItemDao) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(InventoryViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return InventoryViewModel(itemDao) as T
-        }
-        throw IllegalArgumentException("Unknown ViewModel class")
-    }
-}
-     */
 }
 
 class NoteViewModelFactory(private val noteDao: NoteDAO) : ViewModelProvider.Factory {
